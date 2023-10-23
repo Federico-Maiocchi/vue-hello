f1bb34d4f2f5919b2a4b7d18b7e14c748ce1e0f2
Federico-Maiocchi <maiocchifederico@gmail.com>
2023-10-23 14:59:18 +0200

    ADD: stamp objects title in h1

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
 // console.log('Hello Vue')
+
+// Salviamo in una variabile locale la funzione createApp; presa dalla variabile globale Vue
+const { createApp } = Vue;
+
+// Inizializziamo un'istanza dell'aplicazione Vue, invocando la funzione createApp
+createApp ({
+    data() {
+        return {
+            title: 'Hello Vue!'
+        }
+    }
+// invochiamo il metodo .mount( pre connettere l'istanza al tag HTML con id #app e renderizzare l'app
+}).mount('#app')
